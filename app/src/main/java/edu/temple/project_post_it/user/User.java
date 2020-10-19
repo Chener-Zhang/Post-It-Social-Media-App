3344287b3717db7e646c53fa005ba43924305561
chenerzhang <tuh12085@temple.edu>
2020-10-19 12:26:10 -0400

    Create User.java

---
+++ app/src/main/java/edu/temple/project_post_it/user/User.java
@@ -0,0 +1,26 @@
+package edu.temple.project_post_it.user;
+
+import java.util.ArrayList;
+
+public class User {
+    public String user_id;
+    public ArrayList<Object> user_posts;
+
+    public User() {
+
+    }
+
+    public String get_user_id() {
+        return null;
+    }
+
+    public void get_user_post() {
+        
+    }
+
+    public void get_user_group() {
+
+    }
+
+
+}
