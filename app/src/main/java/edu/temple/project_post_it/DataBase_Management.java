d292606f35627df8cb42603ef777aade3c8028c5
chenerzhang <tuh12085@temple.edu>
2020-10-27 15:08:38 -0400

    Connected to fireDatabase

--- app/src/main/java/edu/temple/project_post_it/DataBase_Management.java
+++ app/src/main/java/edu/temple/project_post_it/DataBase_Management.java
@@ -1,7 +1,22 @@
 package edu.temple.project_post_it;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DataBase_Management {
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
     DataBase_Management() {
-        
+        database = FirebaseDatabase.getInstance();
+    }
+
+    public void write_data(String reference, String data) {
+        myRef = database.getReference(reference);
+        myRef.setValue(data);
+    }
+
+    public void get_data() {
+
     }
 }
