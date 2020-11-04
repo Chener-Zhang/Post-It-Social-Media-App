00cbd0e643ae15c318012fffbaf772e2bd82d07a
chenerzhang <tuh12085@temple.edu>
2020-11-03 20:30:49 -0500

    Add delete Data
    + add delete data
    + motify cation on get data
    + documentation of how to use get data and remove data

--- app/src/main/java/edu/temple/project_post_it/DataBase_Management.java
+++ app/src/main/java/edu/temple/project_post_it/DataBase_Management.java
@@ -10,6 +10,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import edu.temple.project_post_it.test.test;
+
 public class DataBase_Management {
     FirebaseDatabase rootNode;
     DatabaseReference databaseReference;
@@ -18,8 +20,9 @@ public class DataBase_Management {
         rootNode = FirebaseDatabase.getInstance();
     }
 
-    public void write_data_child(String child_table, String child_reference, Object object) {
-        databaseReference = rootNode.getReference().child(child_table);
+
+    public void write_data_child(String childs_parent_reference, String child_reference, Object object) {
+        databaseReference = rootNode.getReference().child(childs_parent_reference);
         databaseReference.child(child_reference).setValue(object);
     }
 
@@ -28,21 +31,50 @@ public class DataBase_Management {
         databaseReference.push().setValue(object);
     }
 
+
+    //The reference will be the table directly below postit-8d9a4;
+    /*
+    For example:
+    reference = "Posts/post1/image"
+    reference = "Members/user/name"
+    */
     public void get_data(String reference) {
         databaseReference = rootNode.getReference(reference);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value = snapshot.getValue(String.class);
-                Log.d("TAG", "Value is: " + value);
+                //Class of object, exp : Post place here
+                test object = snapshot.getValue(test.class);
+                //Access the attribute inside the object. Example: object.name, object.image_url
+                Log.d("TAG", "Value is: " + object.getAge());
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Log.w("TAG", "Failed to read value.", error.toException());
-
             }
         });
     }
 
+
+    /* remove data, the format of reference will be same as get data
+    For example:
+    reference = "Posts/post1/image"
+    reference = "Members/user/name"
+    */
+
+    public void remove_data(String reference) {
+        databaseReference = rootNode.getReference(reference);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                snapshot.getRef().removeValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
 }
---
+++ app/src/main/java/edu/temple/project_post_it/test/test.java
@@ -0,0 +1,53 @@
+package edu.temple.project_post_it.test;
+
+public class test {
+    String name;
+    String color;
+    String hobby;
+    String age;
+    String sex;
+
+    public test() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
