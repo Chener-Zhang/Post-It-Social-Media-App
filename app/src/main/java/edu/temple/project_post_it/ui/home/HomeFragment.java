f2af1fb51510bdcc7d1856d8456398c13b15b06f
Darnell Faison <tuh04152@temple.edu>
2020-11-18 18:03:55 -0500

    Added Basic HomeFragment UI.

--- app/src/main/java/edu/temple/project_post_it/ui/home/HomeFragment.java
+++ app/src/main/java/edu/temple/project_post_it/ui/home/HomeFragment.java
@@ -22,7 +22,6 @@ public class HomeFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
 
         return root;
     }
