0a6faa71388242721d653279a9f614f524ac83f5
Darnell Faison <tuh04152@temple.edu>
2020-11-19 15:21:37 -0500

    Altered Post Objects to better fit creation scheme. Added TextPostCreation.

--- app/src/main/java/edu/temple/project_post_it/post/AudioPost.java
+++ app/src/main/java/edu/temple/project_post_it/post/AudioPost.java
@@ -2,12 +2,14 @@ package edu.temple.project_post_it.post;
 
 import android.location.Location;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import java.io.File;
 
 public class AudioPost extends Post {
     File audioFile;
-    public AudioPost(long Post_ID, Location location, int Privacy, int type, File audioFile) {
-        super(Post_ID, location, Privacy, type);
+    public AudioPost(String Post_ID, boolean Privacy, int type, File audioFile) {
+        super(Post_ID, Privacy, type);
         this.audioFile = audioFile;
     }
 }
--- app/src/main/java/edu/temple/project_post_it/ui/home/PostCreationFragment.java
+++ app/src/main/java/edu/temple/project_post_it/ui/home/PostCreationFragment.java
@@ -1,21 +1,46 @@
 package edu.temple.project_post_it.ui.home;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Calendar;
 
 import edu.temple.project_post_it.R;
+import edu.temple.project_post_it.post.Post;
+
+import static edu.temple.project_post_it.CONSTANT.LOCATION_BROADCAST;
+import static edu.temple.project_post_it.CONSTANT.LOCATION_KEY;
 
 public class PostCreationFragment extends Fragment {
 
     private static final String MODE = "MODE";
     private int mode;
+    TextView titleView;
+    String title;
+    TextView descriptionView;
+    String descritpion;
+    CheckBox privacySwitch;
+    boolean isPublic;
+    Button createPostButton;
+    Location location;
+    LatLng latLng;
+    FirebaseUser currentUser;
 
     public PostCreationFragment() {
         // Required empty public constructor
@@ -34,14 +59,67 @@ public class PostCreationFragment extends Fragment {
         if (getArguments() != null) {
             mode = getArguments().getInt(MODE);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_creation, container, false);
+        View view = inflater.inflate(R.layout.fragment_post_creation, container, false);
+        title = "Untitled";
+        descritpion = "No Description";
+        isPublic = true;
+        titleView = view.findViewById(R.id.titleEditText);
+        descriptionView = view.findViewById(R.id.descriptionEditText);
+        privacySwitch = view.findViewById(R.id.privacyCheckBox);
+        createPostButton = view.findViewById(R.id.createPostButton);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                //Get the data from the MapService
+                if (intent.getAction().equals(LOCATION_BROADCAST)) {
+                    location = intent.getParcelableExtra(LOCATION_KEY);
+                    latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                }
+            }
+        };
+
+        createPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String titleTest = titleView.getText().toString();
+                String descriptionTest = (String) descriptionView.getText().toString();
+                if (titleTest.length() > 0){
+                    title = titleTest;
+                }
+                if (descriptionTest.length() > 0){
+                    descritpion = descriptionTest;
+                }
+                if (privacySwitch.isChecked()){
+                    isPublic = false;
+                }
+                String post_id = Calendar.getInstance().getTime().toString() + currentUser.getUid();
+                Post post = new Post(post_id, isPublic, 0);
+                post.setTitle(title);
+                post.setText(descritpion);
+                if (latLng != null){
+                    post.setLocation(latLng);
+                }
+                savePost();
+
+            }
+        });
+
+
+
+        return view;
     }
 
+    public static void savePost(){
+        //This method is where the new post will be saved to the database. This method, when called, will also return the user back to the homepage.
+
+    }
 
 }
