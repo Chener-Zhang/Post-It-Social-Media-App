dc1cc44d75e4ed2cca49ab941a0eb8b4b2fd1c9b
Darnell Faison <tuh04152@temple.edu>
2020-11-23 13:11:49 -0500

    Removed CameraRunner. Allowed creation of photo posts.

--- app/src/main/java/edu/temple/project_post_it/ui/home/ImageCreationFragment.java
+++ app/src/main/java/edu/temple/project_post_it/ui/home/ImageCreationFragment.java
@@ -1,14 +1,40 @@
 package edu.temple.project_post_it.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import edu.temple.project_post_it.R;
+import edu.temple.project_post_it.post.Post;
+import edu.temple.project_post_it.user_navigation;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,33 +43,34 @@ import edu.temple.project_post_it.R;
  */
 public class ImageCreationFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String MODE = "MODE";
+    private int mode;
+    TextView titleView;
+    String title;
+    TextView descriptionView;
+    String descritpion;
+    CheckBox privacySwitch;
+    boolean isPublic;
+    Button createPostButton;
+    Button takePhotoButton;
+    Location location;
+    LatLng latLng;
+    FirebaseUser currentUser;
+    Activity activity;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Uri imageUri;
+
+    String currentPhotoPath;
+    static Uri photoURI;
+    static final int REQUEST_TAKE_PHOTO = 713;
 
     public ImageCreationFragment() {
         // Required empty public constructor
     }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ImageCreationFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ImageCreationFragment newInstance(String param1, String param2) {
+    public static ImageCreationFragment newInstance(int mode) {
         ImageCreationFragment fragment = new ImageCreationFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(MODE, mode);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +79,108 @@ public class ImageCreationFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            mode = getArguments().getInt(MODE);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image_creation, container, false);
+        View view = inflater.inflate(R.layout.fragment_image_creation, container, false);
+        title = "Untitled";
+        descritpion = "No Description";
+        isPublic = true;
+        titleView = view.findViewById(R.id.titleEditText);
+        descriptionView = view.findViewById(R.id.descriptionEditText);
+        privacySwitch = view.findViewById(R.id.privacyCheckBox);
+        createPostButton = view.findViewById(R.id.createPostButton);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (user_navigation.loc != null){
+            latLng = user_navigation.loc;
+        }
+        activity = getActivity();
+        takePhotoButton = view.findViewById(R.id.takePictureButton);
+
+
+        takePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    takePhoto();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+        createPostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String titleTest = titleView.getText().toString();
+                String descriptionTest = (String) descriptionView.getText().toString();
+                if (titleTest.length() > 0){
+                    title = titleTest;
+                }
+                if (descriptionTest.length() > 0){
+                    descritpion = descriptionTest;
+                }
+                if (privacySwitch.isChecked()){
+                    isPublic = false;
+                }
+                String post_id = Calendar.getInstance().getTime().toString() + currentUser.getUid();
+                Post post = new Post(post_id, isPublic, 0);
+                post.setTitle(title);
+                post.setText(descritpion);
+                if (latLng != null){
+                    post.setLocation(latLng);
+                }
+                savePost();
+
+            }
+        });
+
+
+
+        return view;
     }
+
+    public static void savePost(){
+        //This method is where the new post will be saved to the database. This method, when called, will also return the user back to the homepage.
+
+    }
+
+    public void takePhoto() throws IOException {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = createImageFile();
+        imageUri = FileProvider.getUriForFile(getContext(), "edu.temple.project_post_it.fileprovider", photo);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "PNG_" + timeStamp + "_";
+        File storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".png",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            Toast.makeText(this.getContext(), "Photo Taken!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
