38c8e60cc276ca8a8e7f7a034ed251517ad8b6e4
Phaceial <Phaceial@gmail.com>
2020-12-05 15:36:28 -0500

    Fixed bug where private posts where posting to the group. Made privacy variables clearer to understand true = private

--- app/src/main/java/edu/temple/project_post_it/CONSTANT.java
+++ app/src/main/java/edu/temple/project_post_it/CONSTANT.java
@@ -5,16 +5,4 @@ package edu.temple.project_post_it;
 public class CONSTANT {
     public static String LOCATION_BROADCAST = "Location Broadcast";
     public static String LOCATION_KEY = "Location Key";
-    public static String POST_ID = "post id";
-    public static String LOCATION = "location";
-    public static String GROUP_ID = "group id";
-    public static String PRIVACY = "privacy";
-    public static String TEXT = "text";
-    public static String TYPE = "type";
-    public static String USERS = "users";
-    public static String POSTS = "posts";
-    public static String GROUPNAME = "group_name";
-    public static String GROUPLIST = "group_list";
-    public static String POSTLIST = "post_list";
-    public static String USERID = "user_id";
 }
