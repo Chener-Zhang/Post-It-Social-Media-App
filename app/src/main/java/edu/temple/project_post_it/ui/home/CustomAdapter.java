e7652407e992e955a3f95a6732de6c1a60cc61e1
Phaceial <Phaceial@gmail.com>
2020-12-04 22:27:37 -0500

    Revert "Revert "Merge branch 'chener_branch' of https://github.com/tuh12085/PostIt into chener_branch""
    This reverts commit 39a9cb290f72dee70a10c15202c0fa2397af5e4a.

--- app/src/main/java/edu/temple/project_post_it/ui/home/CustomAdapter.java
+++ app/src/main/java/edu/temple/project_post_it/ui/home/CustomAdapter.java
@@ -45,7 +45,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
             public void onClick(View v) {
                 //write the delete code here
                 String current_post = post_list.get(position).getPost_ID();
-                dataBaseManagement.databaseRemoveData(current_post);
+                dataBaseManagement.databaseRemovePostData(current_post);
             }
         });
 
